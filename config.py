e69b1abe3b60812df88c57c75660841267eb36c6
Fabricio <10428537@mackenzista.com.br>
2026-05-23 12:13:07 -0300

    Add default values to config variables
    Updated configuration to include default values for SECRET_KEY and SQLALCHEMY_DATABASE_URI.

--- config.py
+++ config.py
@@ -1,12 +1,25 @@
 import os
 from dotenv import load_dotenv
 
+# Carrega variáveis do arquivo .env
 load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    # =========================================================
+    # CHAVE SECRETA DO FLASK
+    # =========================================================
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    # =========================================================
+    # CONEXÃO COM O POSTGRESQL DA AZURE
+    # =========================================================
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql+psycopg2://adminagrolink@agrolink:%2BRavy123@agrolink.postgres.database.azure.com:5432/postgres?sslmode=require"
+    )
 
+    # =========================================================
+    # DESATIVA MODIFICAÇÕES DO SQLALCHEMY
+    # =========================================================
     SQLALCHEMY_TRACK_MODIFICATIONS = False
